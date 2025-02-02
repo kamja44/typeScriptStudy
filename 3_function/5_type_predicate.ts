c161de67c2c3595267e69608af7ebc0c0aafdb24
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-03 05:26:55 +0900

    Type Predicate

---
+++ 3_function/5_type_predicate.ts
@@ -0,0 +1,54 @@
+/**
+ * Type Predicate
+ */
+function isNumber(input: any): input is number {
+  return typeof input === "number";
+}
+
+console.log(isNumber(10));
+
+function isNumberRetBool(input: any): boolean {
+  return typeof input === "number";
+}
+
+let number: any = 5;
+
+if (isNumberRetBool(number)) {
+  number; // any
+}
+
+if (isNumber(number)) {
+  number; // number
+}
+
+interface Doge {
+  name: string;
+  age: number;
+}
+
+interface Cat {
+  name: string;
+  breed: string;
+}
+
+type DogeOrCat = Doge | Cat;
+
+function isDoge(animal: DogeOrCat): animal is Doge {
+  return (animal as Doge).age !== undefined;
+}
+
+const doge: DogeOrCat =
+  Math.random() > 0.5
+    ? {
+        name: "도지",
+        age: 20,
+      }
+    : {
+        name: "오리",
+        breed: "감자",
+      };
+if (isDoge(doge)) {
+  doge; // Doge
+} else {
+  doge; // Cat
+}
