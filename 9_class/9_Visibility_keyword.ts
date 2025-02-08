4a99ca0cd73b9e22aa188bb8327df1d27e909056
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-08 17:37:23 +0900

    Visibility Keyword

---
+++ 9_class/9_Visibility_keyword.ts
@@ -0,0 +1,41 @@
+/**
+ * Visibility Keyword
+ *
+ * 1) public (기본값)
+ * - 어디서든 접근이 가능하다.
+ *
+ * 2) protected
+ * - 현재 클래스 및 하위(자식) 클래스에서 접근 가능하다.
+ *
+ * 3) private
+ * - 현재 클래스 내부에서만 접근이 가능하다.
+ */
+
+class PropertyTestParent {
+  public publicProperty = "public property";
+  protected protectedProperty = "protected property";
+  private privateProperty = "private property";
+  // #은 Js에서 존재하는 문법 위의 3개는 TS에서만 존재
+  #jsPrivateProperty = `js private Property`;
+
+  test() {
+    this.publicProperty;
+    this.protectedProperty;
+    this.privateProperty;
+    this.#jsPrivateProperty;
+  }
+}
+
+class PropertyTestChild extends PropertyTestParent {
+  test() {
+    this.publicProperty;
+    this.protectedProperty;
+    // private은 자식에서 접근 불가능
+    //     this.privateProperty;
+    //     this.#jsPrivateProperty;
+  }
+}
+
+const instance = new PropertyTestChild();
+
+instance.publicProperty;
