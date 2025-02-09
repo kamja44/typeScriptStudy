7e2b464f4cb0b3e0d247cb865c1bd093cf4e1422
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-09 18:45:13 +0900

    Export Default

---
+++ 15_import_and_export/1_import_default_2.ts
@@ -0,0 +1,16 @@
+import Example from "./1_export_default_1";
+
+// const iu = new IdolModel("아이유", 25);
+// console.log(iu);
+
+// console.log(IdolModel);
+
+// const cat: IdolModel = {
+//   name: "냥냥이",
+//   breed: "길냥이",
+// };
+
+const yuJin = new Example.IdolModel("안유진", 23);
+
+console.log(yuJin);
+console.log(Example.number);
