1ca9c1b0cae4bfc318053c471e96d36170a5e4e2
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-02-09 18:49:05 +0900

    Export

---
+++ 15_import_and_export/2_import_2.ts
@@ -0,0 +1,12 @@
+import { IdolModel, rose, number, ICat } from "./2_export_1";
+
+const iu = new IdolModel("아이유", 23);
+console.log(iu);
+console.log(rose);
+console.log(number);
+
+const cat: ICat = {
+  name: "냥이",
+  age: 12,
+};
+console.log(cat);
